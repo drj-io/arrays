2502b8417cd8180789480c24a0a646b24100c64d
David R. Jones <drj@rackfx.com>
2016-03-08 19:14:17 -0700

    more fruit! yum!

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 var fruits = ["apple","kiwi","oranges","strawberry","mango","kumquat"];
 var moreFruits = ["raspberry", "Pineapple","grape"];
 
-var otherFruit = ["Dragon Fruit"];
+var otherFruit = ["Dragon Fruit", "Plum"];
 
 var allFruits = fruits.concat(moreFruits);
 
-allFruits.push(otherFruit);
+//allFruits.push(otherFruit.toString());
+
+
+otherFruit.forEach(function(fruit){
+  allFruits.push(fruit);
+})
 
 console.log(allFruits.sort().reverse());
 
-
 //console.log(getTheLength(fruits));
 
-
 function getTheLength(a){
-
     return a.length
 }
